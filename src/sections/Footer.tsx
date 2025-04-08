0072669c2bfefd248c2a83db535fdfe6f5dd8ab8
Karan Kadyan <113244995+karankadyan@users.noreply.github.com>
2025-04-08 17:25:39 +0530

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     return (
         <footer className="relative z-0 overflow-x-clip">
             <div
-                className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(40%_40%_at_bottom_center,black,transparent)] -z-10"></div>
+                className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(40%_40%_at_bottom_center,black,transparent)] -z-0"></div>
             <div className="container">
                 <div
                     className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
