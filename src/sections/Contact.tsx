408be1d3733eda8504575b9313f0d23079e47425
Karan Kadyan <113244995+karankadyan@users.noreply.github.com>
2025-04-08 17:38:25 +0530

    Update Contact.tsx

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,11 +5,11 @@ import SectionHeader from "@/components/SectionHeader";
 export const ContactSection = () => {
     return (
         <section id="contact">
-            <div className="py-16 pt-12 lg:pd-24 lg:pt-20 z-0">
+            <div className="py-16 pt-12 lg:pd-24 lg:pt-20">
                 <div className="container">
                     <SectionHeader title={" "} eyebrow={"Connect with me "} description={""}/>
-                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
-                        <div className="absolute inset-0 opacity-5 -z-10"
+                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-40">
+                        <div className="absolute inset-0 opacity-5 pointer-events-none -z-10"
                              style={{backgroundImage: `url(${grainImage.src})`}}/>
                         <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center w-3/2">
                             <div>
